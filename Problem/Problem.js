148b955aab5e35531b679b32dc7205c5477e31b2
antnose <antnose28@gmail.com>
2025-04-12 04:42:25 +0600

    complete 2 problem

---
+++ Problem/Problem.js
@@ -0,0 +1,16 @@
+// # First Problem (01) => Print random intiger number.
+
+// This is the one way
+// function printRandomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// console.log(printRandomNumber(1, 6));
+
+// function printRandomNumber(min, max) {
+//   return Math.ceil(Math.random() * (max - min + 1));
+// }
+// console.log(printRandomNumber(1, 6));
+
+// # Second Problem (02) => Sort an array  alphabetically
+// const students = ["Sumit", "Saad", "Rumi", "Anik", "Rahat"];
+// console.log(students.sort());  // Note: sort method are replace the main array!
